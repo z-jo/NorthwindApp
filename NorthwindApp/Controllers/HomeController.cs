78f40fb4348e4912d916f6017782da9f42d6cd69
z-jo <zoranamjovanovic@gmail.com>
2020-10-26 11:46:07 +0100

    Add project files.

---
+++ NorthwindApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNetCore.Cors;
+using NorthwindApp_DAL;
+
+namespace NorthwindApp.Controllers
+{
+    [EnableCors("AllowSpecificOrigin")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult GetEmployeeList()
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            {
+                var custList = (from e in db.Employees
+                                select new Models.EmployeeModel
+                                {
+                                    LastName = e.LastName,
+                                    FirstName = e.FirstName,
+                                    Title = e.Title,
+                                    Address = e.Address,
+                                    City = e.City,
+                                    Region = e.Region,
+                                    PostalCode = e.PostalCode,
+                                    Country = e.Country
+                                }).ToList();
+
+                return Json(new { data = custList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ NorthwindApp/Models/EmployeeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NorthwindApp_DAL;
+
+namespace NorthwindApp.Models
+{
+    public class EmployeeModel
+    {
+        public string LastName { set; get; }
+        public string FirstName { set; get; }
+        public string Title { set; get; }
+        public string Address { set; get; }
+        public string City { set; get; }
+        public string Region { set; get; }
+        public string PostalCode { set; get; }
+        public string Country { set; get; }
+
+
+    }
+}
