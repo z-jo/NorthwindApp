fec5e6413bbf5329103d624ce0d19415105525d6
z-jo <zoranamjovanovic@gmail.com>
2020-10-30 11:45:05 +0100

    /

---
+++ NorthwindApp/Controllers/DatesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NorthwindApp.Models;
+using NorthwindApp_DAL;
+
+namespace NorthwindApp.Controllers
+{
+    public class DatesController : Controller
+    {
+        // GET: Dates
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+        public ActionResult SubmitDates(DatesModel dates)
+        {
+
+            return Json(new { data = "zorana" }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult GetDates()
+        {
+
+            //return View();
+            return Json(new { data = "zorana" }, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ NorthwindApp/Models/DatesModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NorthwindApp_DAL;
+
+namespace NorthwindApp.Models
+{
+    public class DatesModel
+    {
+        public int EmployeeID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+    }
+}
